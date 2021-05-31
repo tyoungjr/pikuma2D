8ba258a8bf46938d4bff5292e0f9f71672e8d414
T <tmoney@pop-os.localdomain>
2021-05-31 08:47:18 -0500

    Initial commit from pikuma

---
+++ src/Game.cpp
@@ -0,0 +1,35 @@
+#include "Game.h"
+#include <SDL2/SDL.h>
+#include <iostream>
+
+Game::Game() {
+  // TODO...
+  std::cout << "Game constructor called" << std::endl;
+}
+
+Game::~Game() { std::cout << "Game destructor called" << std::endl; }
+
+void Game::Initialize() {
+  if(SDL_Init(SDL_INIT_EVERYTHING) != 0){
+    std::cerr << "Error initialializing SDL" << std::endl;
+    return;
+  }
+
+  SDL_Window* window = SDL_CreateWindow
+}
+
+void Game::Run() {
+  // while (true) {
+  //   ProcessInput();
+  //   Update();
+  //   Render();
+  // }
+}
+
+void Game::ProcessInput() {}
+
+void Game::Update() {}
+
+void Game::Destroy() {}
+
+void Game::Render() {}
