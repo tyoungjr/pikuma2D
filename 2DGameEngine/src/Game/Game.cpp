380af2d670454f7584d18b584e894625da2fd08a
tmoney <tyoungjr@boargames.xyz>
2021-12-15 18:15:29 -0600

    Updating SDL Path Adding CRT macro to ignore warnings for now Fixing formatting on some of the code.

--- 2DGameEngine/src/Game/Game.cpp
+++ 2DGameEngine/src/Game/Game.cpp
@@ -7,108 +7,107 @@
 #include <iostream>
 
 Game::Game() {
-  isRunning = false;
-  Logger::Log("Game constructor called!");
+	isRunning = false;
+	Logger::Log("Game constructor called!");
 }
 
 Game::~Game() { Logger::Log("Game destructor called"); }
 
 void Game::Initialize() {
-  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-    Logger::Err("Error initializing SDL");
-    return;
-  }
-  // get the display mode from the system, setting it to full
-  SDL_DisplayMode displayMode;
-  SDL_GetCurrentDisplayMode(0, &displayMode);
-  windowWidth = 960;
-  windowHeight = 720;
-  window =
-      SDL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                       windowWidth, windowHeight, 0);
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		Logger::Err("Error initializing SDL");
+		return;
+	}
+	// get the display mode from the system, setting it to full
+	SDL_DisplayMode displayMode;
+	SDL_GetCurrentDisplayMode(0, &displayMode);
+	windowWidth = 960;
+	windowHeight = 720;
+	window = SDL_CreateWindow("Pikuma 2D Engine Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		windowWidth, windowHeight, 0);
 
-  if (!window) {
-    Logger::Err("Error creating SDL window.");
-    return;
-  }
+	if (!window) {
+		Logger::Err("Error creating SDL window.");
+		return;
+	}
 
-  // window pointer , display index and uflags
-  renderer = SDL_CreateRenderer(window, -1, 0);
-  if (!renderer) {
-    Logger::Err("Error creating SDL Renderer");
-    return;
-  }
- /* SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);*/
-  isRunning = true;
+	// window pointer , display index and uflags
+	renderer = SDL_CreateRenderer(window, -1, 0);
+	if (!renderer) {
+		Logger::Err("Error creating SDL Renderer");
+		return;
+	}
+
+	isRunning = true;
 }
 
 void Game::Setup() {
-  // TODO:
-  // Entity tank = registry.CreateEntity();
-  // tank.AddComponent<TransformComponent>();
-  // tank.AddComponent<BoxColliderComponent>();
-  // tank.AddComonent<SpriteComonent>(".assets/images/tank.png");
+	// TODO:
+	// Entity tank = registry.CreateEntity();
+	// tank.AddComponent<TransformComponent>();
+	// tank.AddComponent<BoxColliderComponent>();
+	// tank.AddComonent<SpriteComonent>(".assets/images/tank.png");
 }
 
 void Game::Run() {
-  Setup();
-  while (isRunning) {
-    ProcessInput();
-    Update();
-    Render();
-  }
+	Setup();
+	while (isRunning) {
+		ProcessInput();
+		Update();
+		Render();
+	}
 }
 
 void Game::ProcessInput() {
 
-  SDL_Event sdlEvent;
-  while (SDL_PollEvent(&sdlEvent)) {
-    switch (sdlEvent.type) {
-    case SDL_QUIT:
-      isRunning = false;
-      break;
-    case SDL_KEYDOWN:
-      if (sdlEvent.key.keysym.sym == SDLK_ESCAPE) {
-        isRunning = false;
-      }
-      break;
-    }
-  }
+	SDL_Event sdlEvent;
+	while (SDL_PollEvent(&sdlEvent)) {
+		switch (sdlEvent.type) {
+		case SDL_QUIT:
+			isRunning = false;
+			break;
+		case SDL_KEYDOWN:
+			if (sdlEvent.key.keysym.sym == SDLK_ESCAPE) {
+				isRunning = false;
+			}
+			break;
+		}
+	}
 }
 
 void Game::Update() {
-  // if we are too fast , release execution until we reach the
-  // MILLISECS_PER_FRAME
-  int timeToWait =
-      MILLISECS_PER_FRAME - (SDL_GetTicks() - millisecsPreviousFrame);
+	// if we are too fast , release execution until we reach the
+	// MILLISECS_PER_FRAME
+	int timeToWait =
+		MILLISECS_PER_FRAME - (SDL_GetTicks() - millisecsPreviousFrame);
 
-  if (timeToWait > 0 && timeToWait <= MILLISECS_PER_FRAME) {
-    SDL_Delay(timeToWait);
-  }
+	if (timeToWait > 0 && timeToWait <= MILLISECS_PER_FRAME) {
+		SDL_Delay(timeToWait);
+	}
 
-  // the difference in ticks since the last frame, converted to seconds
-  double deltaTime = (SDL_GetTicks() - millisecsPreviousFrame) / 1000.0;
+	// the difference in ticks since the last frame, converted to seconds
+	double deltaTime = (SDL_GetTicks() - millisecsPreviousFrame) / 1000.0;
 
-  // store the "previous" frame time
-  millisecsPreviousFrame = SDL_GetTicks();
+	// store the "previous" frame time
+	millisecsPreviousFrame = SDL_GetTicks();
 
-  // TODO:
-  // MovementSystem.Update();
-  // CollisionSystem.Update();
-  // DamageSystem.Update();
+	// TODO:
+	// MovementSystem.Update();
+	// CollisionSystem.Update();
+	// DamageSystem.Update();
 }
 
 void Game::Destroy() {
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 }
 
 void Game::Render() {
-  SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
-  SDL_RenderClear(renderer);
+	SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
+	SDL_RenderClear(renderer);
 
-  // TODO: Render game object
+	// TODO: Render game object
 
-  SDL_RenderPresent(renderer);
+	SDL_RenderPresent(renderer);
 }
--- 2DGameEngine/src/Logger/Logger.cpp
+++ 2DGameEngine/src/Logger/Logger.cpp
@@ -5,36 +5,33 @@
 #include <iostream>
 #include <string>
 
-
-
 std::vector<LogEntry> Logger::messages;
-// why C is important
+
 std::string CurrentDateTimeToString() {
-  std::time_t now =
-      std::chrono::system_clock::to_time_t(std::chrono::system_clock ::now());
-  std::string output(30, '\0');
+    std::time_t now =
+    std::chrono::system_clock::to_time_t(std::chrono::system_clock ::now());
+    std::string output(30, '\0');
 
-  // localtime is unsafe but idk what to change it to
-  // see https://docs.microsoft.com/en-us/cpp/error-messages/compiler-warnings/compiler-warning-level-3-c4996?view=msvc-170`
-  std::strftime(&output[0], output.size(), "%d-%b-%Y %H:%M:%S",
-                std::localtime(&now));
+    // TODO: localtime is unsafe but idk what to change it to
+    // see https://docs.microsoft.com/en-us/cpp/error-messages/compiler-warnings/compiler-warning-level-3-c4996?view=msvc-170`
+    std::strftime(&output[0], output.size(), "%d-%b-%Y %H:%M:%S", std::localtime(&now));
 
-  return output;
+    return output;
 }
 void Logger::Log(const std::string &message) {
-  LogEntry logEntry;
-  logEntry.type = LOG_INFO;
-  logEntry.message = "LOG: [" + CurrentDateTimeToString() + "]" + message;
+    LogEntry logEntry;
+    logEntry.type = LOG_INFO;
+    logEntry.message = "LOG: [" + CurrentDateTimeToString() + "]" + message;
 
-  std::cout << "\x1B[32m" << logEntry.message << "\033[0m" << std::endl;
-  messages.push_back(logEntry);
+    std::cout << "\x1B[32m" << logEntry.message << "\033[0m" << std::endl;
+    messages.push_back(logEntry);
 }
 
 void Logger::Err(const std::string &message) {
-  LogEntry logEntry;
-  logEntry.type = LOG_ERROR;
-  logEntry.message = "ERR: [" + CurrentDateTimeToString() + "]" + message;
+	LogEntry logEntry;
+	logEntry.type = LOG_ERROR;
+	logEntry.message = "ERR: [" + CurrentDateTimeToString() + "]" + message;
 
-  std::cerr << "\x1B[91m" << logEntry.message << "\033[0m" << std::endl;
-  messages.push_back(logEntry);
+	std::cerr << "\x1B[91m" << logEntry.message << "\033[0m" << std::endl;
+	messages.push_back(logEntry);
 }
