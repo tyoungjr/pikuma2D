6e92ee1a6c1b4e38eb7343d1d5917b39884c17ca
tmoney <tyoungjr@boargames.xyz>
2022-02-27 17:42:19 -0600

    Adding operator overloads to Entity class

--- include/ECS/ECS.h
+++ include/ECS/ECS.h
@@ -34,7 +34,15 @@ private:
 
 public:
 	Entity(int id) : id(id) {};
+	Entity(const Entity& entity) = default; 
+	
 	int GetId() const;
+
+	Entity& operator =(const Entity& other) = default; 
+	bool operator ==(const Entity& other) const { return id == other.id; } 
+	bool operator !=(const Entity& other) const { return id != other.id; }
+	bool operator >(const Entity& other) const { return id > other.id; }
+	bool operator <(const Entity& other) const { return id < other.id; }
 
 };
 
